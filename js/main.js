53476faa4756dce5132781f3d327afe0a95f89ae
Sergi <sergicrujera17@gmail.com>
2018-05-08 11:59:57 +0200

    Primer commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  console.log("Web ready");
+  var var1="jquery";
+  var var2="javascript";
+  if(var1 == var2){
+      console.log("Son iguals");
+  }else {
+  console.log("No son iguals");
+  }
+
+  var coche=["Ford","Mustang","Seat","Nissan","Mercedes"]
+  for (var i = 0; i < coche.length; i++) {
+      console.log(coche[i]);
+  }
+});
